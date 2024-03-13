970d75c33615fe2f396f9088b4f7ceea6774409b
Fabian <garciafabianemmanuel@gmail.com>
2024-03-13 10:55:28 -0300

    factorial resuelto

---
+++ factorial.py
@@ -0,0 +1,29 @@
+# Calcular el factorial de n sin *
+
+def suma(a, b):
+    return a +b
+
+def multi(a, b):
+    resultado = 0
+    for _ in range(b):
+        resultado = suma(resultado, a)
+    return resultado
+
+def factorial(num):
+    if num == 0:
+        return 1
+    resultado = 1
+    for i in range(1, num + 1):
+        resultado = multi(resultado, i)
+    return resultado
+
+
+print('Ingrese un numero para realizar su factorial')
+num = int(input())
+resultado = factorial(num)
+print(f'El factorial es {num} es: {resultado}')
+
+
+
+
+
